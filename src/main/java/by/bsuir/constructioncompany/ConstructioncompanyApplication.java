2623621d19c3de415cf48c3f9608114f2032bb0f
VladislavKortokov <vladislav.korotkow@gmail.com>
2024-02-24 21:09:30 +0300

    Initial commit

---
+++ src/main/java/by/bsuir/constructioncompany/ConstructioncompanyApplication.java
@@ -0,0 +1,13 @@
+package by.bsuir.constructioncompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructioncompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstructioncompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/bsuir/constructioncompany/ConstructioncompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsuir.constructioncompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructioncompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
