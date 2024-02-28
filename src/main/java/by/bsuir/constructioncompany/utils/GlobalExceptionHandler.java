f0d39f123cd3db2f31bdcbc24e329985d60d13b8
VladislavKortokov <vladislav.korotkow@gmail.com>
2024-02-28 10:43:55 +0300

    add GlobalExceptionHandler

---
+++ src/main/java/by/bsuir/constructioncompany/utils/GlobalExceptionHandler.java
@@ -0,0 +1,54 @@
+package by.bsuir.constructioncompany.utils;
+
+
+import by.bsuir.constructioncompany.exceptions.CannotBeDeletedException;
+import by.bsuir.constructioncompany.exceptions.ObjectNotFoundException;
+import by.bsuir.constructioncompany.exceptions.UserAlreadyExistsException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@RestControllerAdvice
+public class GlobalExceptionHandler{
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleException(MethodArgumentNotValidException ex, WebRequest req){
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<String> handleObjectNotFoundException(ObjectNotFoundException objectNotFoundException){
+        return new ResponseEntity<>(objectNotFoundException.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<String> handleSQLIntegrityConstraintViolationException(Exception exception ){
+        return new ResponseEntity<>("SQL error", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<String> handleUserAlreadyExistsException(UserAlreadyExistsException userAlreadyExistsException){
+        return new ResponseEntity<>(userAlreadyExistsException.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(CannotBeDeletedException.class)
+    public ResponseEntity<String> handleCannotBeDeletedException(CannotBeDeletedException cannotBeDeletedException){
+        return new ResponseEntity<>(cannotBeDeletedException.getMessage(), HttpStatus.CONFLICT);
+    }
+}
