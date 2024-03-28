c070524315ff598abaed9feb5f1c20573a642f60
VladislavKortokov <vladislav.korotkow@gmail.com>
2024-03-28 22:40:24 +0300

    change jwtTokenRepository

--- src/main/java/by/bsuir/constructioncompany/repositories/JwtRefreshTokenRepository.java
+++ src/main/java/by/bsuir/constructioncompany/repositories/JwtRefreshTokenRepository.java
@@ -3,9 +3,11 @@ package by.bsuir.constructioncompany.repositories;
 import by.bsuir.constructioncompany.models.JwtRefreshToken;
 import by.bsuir.constructioncompany.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface JwtRefreshTokenRepository extends JpaRepository<JwtRefreshToken, Long> {
     void deleteIfExistsByUser(User user);
     Optional<JwtRefreshToken> findByToken(String token);
